332ab6887601378c00c0e337517b801c62f54279
noblet <appiahpatricknoblet@gmail.com>
2024-10-02 04:00:38 +0100

    FEAT: Added the express Router module for application status and statistics

--- routes/index.js
+++ routes/index.js
@@ -7,18 +7,16 @@
  * and retrieving statistics. It utilizes the AppController to handle the
  * requests and responses.
  *
- * @module routes/app
- * @requires express
- * @requires ../controllers/AppController
  */
 
-import Router from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 /**
- * Route to get the status of the application.
+ * Route to get the status of the of redis and mongoBD.
  * @name GET /status
  * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
@@ -34,5 +32,14 @@ router.get('/status', async (request, response) => AppController.getStatus(reque
  * @returns {Promise<void>} A promise that resolves when the response is sent.
  */
 router.get('/stats', async (request, response) => AppController.getStats(request, response));
+
+/**
+ * Route to create a new user.
+ * @name POST /users
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * @returns {Promise<void>} A promise that resolves when the response is sent.
+ */
+router.post('users', async (request, response) => UsersController.postNew(request, response));
 
 export default router;
