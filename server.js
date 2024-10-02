df1bad2cf6e3e18befcbb24848a8230285d2eab8
noblet <appiahpatricknoblet@gmail.com>
2024-10-02 06:14:37 +0100

    FIX: Minor bug fix - moved app.use(express.json()) above app.use(routes) to ensure json middleware is applied before routes are registered

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Use the defined routes
-app.use(routes);
 app.use(express.json());
+app.use(routes);
 
 // Starts the server
 app.listen(port, () => {
