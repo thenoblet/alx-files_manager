91e603926c3bef87ac459fad3b97791aa4ced1f2
noblet <appiahpatricknoblet@gmail.com>
2024-10-02 09:30:10 +0100

    ADDED: User controller mechanisms

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
+import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
 import HTTPError from '../utils/httpErrors';
-import hashPassword from '../utils/auth';
+import { hashPassword } from '../utils/auth';
+import redisClient from '../utils/redis';
 
 /**
  * Controller for managing user-related actions.
@@ -30,8 +32,8 @@ class UsersController {
       return HTTPError.badRequest(response, 'Missing password');
     }
 
-    const userEmail = await dbClient.db.collection('users').findOne({ email });
-    if (userEmail) {
+    const existingUser = await dbClient.db.collection('users').findOne({ email });
+    if (existingUser) {
       return HTTPError.badRequest(response, 'Already exist');
     }
 
@@ -42,6 +44,54 @@ class UsersController {
     } catch (error) {
       return HTTPError.internalServerError(response, 'Error: Unable to create user.');
     }
+  }
+
+  /**
+   * Retrieves the current user's data.
+   *
+   * This method checks the authentication token and fetches user data if valid.
+   *
+   * @param {Object} request - The HTTP request object.
+   * @param {Object} response - The HTTP response object.
+   * @returns {Promise<void>} A promise that resolves when the response is sent.
+   */
+  static async getMe(request, response) {
+    try {
+      const user = await this.getUserData(request);
+      if (!user) {
+        return HTTPError.unauthorized(request);
+      }
+
+      return response.status(200).json({ id: user._id, email: user.email });
+    } catch (error) {
+      console.error(`Error fetching user data: ${error}`);
+      return HTTPError.unauthorized(response);
+    }
+  }
+
+  /**
+   * Retrieves user data based on the provided authentication token.
+   *
+   * @param {Object} request - The HTTP request object.
+   * @returns {Promise<Object|null>} The user object if found, or null.
+   */
+  static async getUserData(request) {
+    const userToken = request.get('X-Token');
+    if (!userToken) {
+      throw new Error('Token is missing');
+    }
+
+    const userId = await redisClient.get(`auth_${userToken}`);
+    if (!userId) {
+      return null;
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    return user;
   }
 }
 
